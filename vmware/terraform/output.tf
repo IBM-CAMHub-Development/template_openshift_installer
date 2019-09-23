7824f8163e26aafa71a61290531b306be17b8a3a
CAMHub PublishBot <octravis@us.ibm.com>
2019-09-23 03:50:57 +0000

    Automated CAMHub::publish on 2019-09-23 03:50:57 UTC

--- vmware/terraform/output.tf
+++ vmware/terraform/output.tf
@@ -20,3 +20,23 @@ output "openshift_password" {
 output "openshift_master_ip" {
   value = "${element(values(var.master_node_hostname_ip),0)}"
 }
+
+output "openshift_master_hostname" {
+  value = "${element(keys(var.master_node_hostname_ip),0)}"
+}
+
+output "openshift_infra_ip" {
+  value = "${element(values(var.infra_node_hostname_ip),0)}"
+}
+
+output "openshift_compute1_hostname" {
+  value = "${element(keys(var.compute_node_hostname_ip),0)}"
+}
+
+output "openshift_compute2_hostname" {
+  value = "${length(var.compute_node_hostname_ip) > 1 ? element(keys(var.compute_node_hostname_ip),1) : ""}"
+}
+
+output "openshift_compute3_hostname" {
+  value = "${length(var.compute_node_hostname_ip) > 2 ? element(keys(var.compute_node_hostname_ip),2) : ""}"
+}
